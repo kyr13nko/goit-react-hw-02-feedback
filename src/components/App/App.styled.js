bdc967d7288d4e2e49458cda90f496ca9eec640f
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-09-07 15:28:34 +0300

    add styles

--- src/components/App/App.styled.js
+++ src/components/App/App.styled.js
@@ -5,7 +5,12 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 20px;
+
+  box-shadow: 4px -4px 0px 4px rgba(84, 76, 122, 1),
+    -4px 4px 0px 4px rgba(57, 95, 58, 1);
+
   color: #010101;
+
   padding: 20px;
 `;
 
--- src/components/FeedbackOptions/FeedbackOptions.styled.js
+++ src/components/FeedbackOptions/FeedbackOptions.styled.js
@@ -8,9 +8,21 @@ export const Wrapper = styled.div`
 export const Button = styled.button`
   cursor: pointer;
 
+  background-color: transparent;
+
+  border: none;
+  outline: none;
+
+  box-shadow: 2px -2px 0px 2px rgba(84, 76, 122, 1),
+    -2px 2px 0px 2px rgba(57, 95, 58, 1);
+
   padding: 4px 16px;
 
   &:first-letter {
     text-transform: uppercase;
+  }
+
+  &:hover {
+    background-color: rgba(128, 128, 128, 0.1);
   }
 `;
